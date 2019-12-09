551b99f280303a35584d93321a687a72e635ff7c
benus <benus_ying@hotmail.com>
2019-12-09 12:22:04 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=benus
 
 export MYCLUSTER=tektonknativebeijing36
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing36/kube-config-tok04-tektonknativebeijing36.yml
 
 export NAMESPACE=tektonknativebeijing36
 export EMAIL=benus.ying@gmail.com
